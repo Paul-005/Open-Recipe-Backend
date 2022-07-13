9fe8621940081a82611b7fe99584606edb648565
Paul B <paulbabu05@gmail.com>
2022-07-13 05:24:15 +0000

    change cors permission

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
-const path = require("path");
 
 require("dotenv").config();
 
@@ -21,12 +20,10 @@ const upload = require("./routes/content/file-upload");
 const app = express();
 var port = process.env.PORT || 5000;
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: 100000000 }));
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://open-recipe-paul.vercel.app" }));
 app.use(fileUpload());
-
-app.use(express.static(path.join(__dirname, "public", "Open_Recipe", "build")));
 
 //routes
 app.use(CreateAccount);
@@ -49,7 +46,5 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "public", "Open_Recipe", "build", "index.html")
-  );
+  res.redirect("https://open-recipe-paul.vercel.app");
 });
