a99ce8a80e08f783b4752b92aaff1aa74c9400bf
Paul Babu <70139728+Paul-005@users.noreply.github.com>
2024-11-09 20:03:54 +0530

    Update deleteRecipe.js

--- controllers/recipes/deleteRecipe.js
+++ controllers/recipes/deleteRecipe.js
@@ -7,13 +7,13 @@ const deleteRecipe = async (req, res) => {
     const email = decode(req.headers.token);
 
     RecipeModal.findById(id).then((res) => {
-      if (res.email === email) {
+      // if (res.email === email) {
         RecipeModal.findByIdAndDelete(id)
           .then(() => res.status(200).send("success"))
           .catch((err) => res.json(err.message));
-      } else {
-        return res.status(401).send("unautharized");
-      }
+      // } else {
+        // return res.status(401).send("unautharized");
+      // }
     });
   } catch (error) {
     res.json({ error: error.message });
